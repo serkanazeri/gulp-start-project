f6f414f528268c6a8a2115de86872b4f052dfd36
Serkan Azeri <serkanazeri@gmail.com>
2016-01-30 23:43:11 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+var gulp = require('gulp'),
+    runSequence = require('run-sequence'),
+    concat = require('gulp-concat'),
+    sass = require('gulp-sass'),
+    clean = require('gulp-clean'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    inject = require('gulp-inject'),
+    htmlmin = require('gulp-htmlmin'),
+    browserSync = require('browser-sync');
+
+var src = {
+    sassSrc : 'source/sass/*.sass',
+    scriptsSrc : 'source/scripts/*.js',
+    htmlSrc : 'source/*.html'
+}
+
+
+gulp.task('styles',function(){
+
+    gulp.src(src.sassSrc)
+        .pipe(concat('styles.sass'))
+        .pipe(gulp.dest('app/sass/'))
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.stream());
+
+});
+
+
+gulp.task('scripts',function(){
+
+    gulp.src(src.scriptsSrc)
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('app/js/'))
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/js/'))
+        .pipe(browserSync.stream());
+
+});
+
+
+gulp.task('html',function(){
+
+    gulp.src(src.htmlSrc)
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('app/'))
+        .pipe(browserSync.stream());
+
+});
+
+
+gulp.task('clean',function(){
+
+    gulp.src('app/sass')
+        .pipe(clean())
+
+});
+
+
+gulp.task('inject',function(){
+
+    gulp.src('app/*.html')
+        .pipe(inject(gulp.src(['./app/js/*.js','./app/css/*.css']), {ignorePath: 'app/', addRootSlash: false, relative: false}))
+        .pipe(gulp.dest('app/'))
+        .pipe(browserSync.stream());
+
+});
+
+
+gulp.task('default',function(){
+    runSequence(['styles','scripts','html','clean']);
+});
+
+gulp.task('serve',function(){
+
+    runSequence(['inject']);
+
+    browserSync.init({
+        server: "./app/"
+    });
+
+    gulp.watch('source/sass/*.sass', ['styles']),
+    gulp.watch('source/scripts/*.js', ['scripts']);
+
+});
---
+++ readme.md
@@ -0,0 +1,28 @@
+**Gulp Start Project**
+=======================
+
+Projelerimizde rutin olarak yapmamız gereken işleri gulp ile otomatize ederek hızlı bir şekilde çalışmamızı sağlar.
+
+**Kurulum**
+-----------
+Projemizde Gulp kullanabilmek için öncelikle [node.js](http://nodejs.org) kurulumu yapmamız gerekmektedir.
+
+Node kurulumunu yaptıktan sonra Gulp kurulumu için terminale aşağıdaki komutları yazmamız gerekmektedir.
+
+    npm install -g gulp
+
+Gulp kurulumunu yaptıktan sonra node modülerimizi yüklememiz gerekmektedir.
+
+    npm install
+
+**Kullanım**
+-------
+Node ve Gulp kurulumunu yaptık node modüllerimizi yükledik artık projemizi çalıştırabiliriz.
+
+
+Projemizi çalıştırmak için sırasıyla aşağıdaki komutlarımızı çalıştıralım.
+
+    gulp
+
+    gulp serve
+
